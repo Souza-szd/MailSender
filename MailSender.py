86701ea57403b150387b05529985052ae51001f1
Souza-szd <souzaszd@gmail.com>
2025-02-06 19:06:01 -0300

    Correção

--- MailSender.py
+++ MailSender.py
@@ -23,7 +23,7 @@ def enviar_email(nome, email_destino):
     Meu nome é (nome), sou aluno do curso de Automação Industrial no Colégio Técnico da UFMG (COLTEC), e gostaria de manifestar meu interesse em ingressar em um projeto de iniciação científica sob sua orientação. Tenho grande interesse em ampliar meus conhecimentos na área científica e laboratorial, além de contribuir com as atividades e objetivos do seu laboratório. Estou motivado a aprender novas técnicas, colaborar nas pesquisas e desenvolver habilidades práticas que complementem minha formação acadêmica. Coloco-me à disposição para uma conversa ou entrevista, caso necessário. Agradeço desde já pela atenção e pela oportunidade.
 
 Atenciosamente,
-Arthur Souza Santos
+(aluno)
 Telefone de contato: 31 991077099
     """
     
@@ -42,47 +42,7 @@ Telefone de contato: 31 991077099
 # Lista de destinatários
 
 destinatarios = [
-    {"nome": "Adaises Simone Maciel da Silva", "email": "adaisesmaciel@ufmg.br"},
-    {"nome": "Ana Elisa Cruz Correa", "email": "aecorrea@teiacoltec.org"},
-    {"nome": "Ana Luiza Costa Cruz Borges", "email": "analuiza@vet.ufmg.br"},
-    {"nome": "Ana Maria Alves Saraiva", "email": "anasaraiva@ufmg.br"},
-    {"nome": "Ana Paula de Carvalho Teixeira", "email": "anapaula.cta1@gmail.com"},
-    {"nome": "Ana Paula Meneses Alves", "email": "apmeneses@gmail.com"},
-    {"nome": "Ana Paula Sampaio Caldeira", "email": "anapaula.sampaiocaldeira@gmail.com"},
-    {"nome": "Ana Lucia Brunialti Godard", "email": "brunialt@ufmg.br"},
-    {"nome": "Andre Augusto Gomes Faraco", "email": "andrefaraco@ufmg.br"},
-    {"nome": "Andrey Pereira Lage", "email": "alage@vet.ufmg.br"},
-    {"nome": "Anna Christina de Almeida", "email": "aca2006@ica.ufmg.br"},
-    {"nome": "Anna Paula Vencato", "email": "apvencato@gmail.com"},
-    {"nome": "Antonio Jose Lopes Alves", "email": "ajlacoltec@ufmg.br"},
-    {"nome": "Antonio Pereira Magalhaes Junior", "email": "antonio.magalhaes.ufmg@gmail.com"},
-    {"nome": "Aristobolo Mendes da Silva", "email": "aristobolo@ufmg.br"},
-    {"nome": "Aristobolo Mendes da Silva", "email": "aristobolosilva@gmail.com"},
-    {"nome": "Belisa Vieira da Silveira", "email": "bvsilveira@unaerp.br"},
-    {"nome": "Bernardo de Mattos Viana", "email": "bernardomviana@ufmg.br"},
-    {"nome": "Bernardo de Mattos Viana", "email": "bernardomviana@yahoo.com"},
-    {"nome": "Bernardo Lages Rodrigues", "email": "bernardo@qui.ufmg.br"},
-    {"nome": "Bruna Figueiredo Manzo", "email": "brunaamancio@yahoo.com.br"},
-    {"nome": "Camilla Henriques Maia de Camargos", "email": "camillahmcamargos@gmail.com"},
-    {"nome": "Carla Jorge Machado", "email": "carlajm@ufmg.br"},
-    {"nome": "Carlos Augusto de Oliveira Junior", "email": "carlosoliveira@cca.ufpb.br"},
-    {"nome": "Claudia Regina Vieira", "email": "claudia.vieira@ufabc.edu.br"},
-    {"nome": "Cleiton Lopes Aguiar", "email": "cleiton-aguiar@ufmg.br"},
-    {"nome": "Daniel Fernandes Macedo", "email": "damacedo@dcc.ufmg.br"},
-    {"nome": "Daniela Chemim de Melo", "email": "danichemim@gmail.com"},
-    {"nome": "Daniele Kasper", "email": "daniele@biof.ufrj"},
-    {"nome": "Dawit Albieiro Pinheiro Goncalves", "email": "dawit@ufmg.br"},
-    {"nome": "Dayse Carvalho da Silva Martins", "email": "daysecsm@yahoo.com"},
-    {"nome": "Debora Silvano Moreira", "email": "moreiradebora@yahoo.com"},
-    {"nome": "Demerson Arruda Sanglard", "email": "sanglard@ufmg.br"},
-    {"nome": "Diego Gomes Rocha", "email": "diegogr@ufmg.br"},
-    {"nome": "Diego Gomes Rocha", "email": "diegogr.ufmg@gmail.com"},
-    {"nome": "Diogo Montes Vidal", "email": "vidal@qui.ufmg.br"},
-    {"nome": "Diogo Montes Vidal", "email": "diogomvidal@gmail.com"},
-    {"nome": "Douglas Batista Mazzinghy", "email": "dmazzinghy@demin.ufmg.br"},
-    {"nome": "Elaine Miguel Delvivo Farao", "email": "elainebiofis@yahoo.com.br"},
-    {"nome": "Elaine Miguel Delvivo Farao", "email": "elaine.delvivo@ufms.br"},
-    {"nome": "Elaine Miguel Delvivo Farao", "email": "elainemiguel@ufj.edu.br"},
+    {"nome": "Nome professor", "email": "NomeProfessor@gmail.br"},
 
 ]
 
